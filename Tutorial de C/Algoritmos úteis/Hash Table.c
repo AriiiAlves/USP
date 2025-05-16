53b774a13ac07e63cb309f6dff31dce673016aa8
Ariel Alves <alvzariel@gmail.com>
2025-05-16 10:09:41 -0300

    Update Hash Table.c

--- Tutorial de C/Algoritmos úteis/Hash Table.c
+++ Tutorial de C/Algoritmos úteis/Hash Table.c
@@ -3,21 +3,21 @@
 int main() {
     #define MAX 1000
 
-    /*Lucas e Sofia colecionam figurinhas numeradas de 1 atÈ 1000.
-      Lucas possui algumas repetidas e Sofia est· precisando completar
-      sua coleÁ„o. Escreva um programa que determine quantas figurinhas
+    /*Lucas e Sofia colecionam figurinhas numeradas de 1 at√© 1000.
+      Lucas possui algumas repetidas e Sofia est√° precisando completar
+      sua cole√ß√£o. Escreva um programa que determine quantas figurinhas
       diferentes Lucas pode fornecer a Sofia.*/
 
     /* Exemplo de entrada:
 
-    5 4 -> N˙mero m·ximo de figurinha para Lucas e Sofia
+    5 4 -> N√∫mero m√°ximo de figurinha para Lucas e Sofia
     1 3 5 7 9 -> Figurinhas Lucas
     2 3 4 9 -> Figurinha Sofia */
 
     // FAZENDO POR HASH TABLE
 
-    /* Na hash table, o Ìndice do array representa o prÛprio valor. E se
-       aquele valor est· presente ou n„o, ser· representado por um booleano
+    /* Na hash table, o √≠ndice do array representa o pr√≥prio valor. E se
+       aquele valor est√° presente ou n√£o, ser√° representado por um booleano
        (0 ou 1).
 
        Exemplo: Lucas tem as figurinha 3 e 5.
@@ -34,15 +34,16 @@ int main() {
 
     for(int i = 0; i < maxLucas; i++){
         scanf("%d", &figLucas);
-        Lucas[figLucas]++;
+        Lucas[figLucas]=1;
     }
 
     for(int i = 0; i < maxSofia; i++){
         scanf("%d", &figSofia);
-        Lucas[figSofia]++;
+        Sofia[figSofia]=1;
     }
 
-    for(int i = 0; i < maxLucas; i++){
+    for(int i = 0; i < MAX; i++){
+        //printf("Lucas[%d]: %d\nSofia[%d]: %d\n", i, Lucas[i], i, Sofia[i]);
         if(Lucas[i]==1&&Sofia[i]==0){
             count++;
         }
